6a15f0a7e1a5d765d3231db04cd85cc8b185d049
nandinigupta2207 <113578855+nandinigupta2207@users.noreply.github.com>
2023-04-24 18:37:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#from prediction import predict
+from prediction import predict
 
 st.title("Uncovering Hidden Relationships: Obesity, Lifestyle Expressions")
 st.markdown("FIND YOUR WAY TO HEALTH")
