1535054ed3f0b29b3a117aa9fa4df8bf0efcf0d7
nandinigupta2207 <113578855+nandinigupta2207@users.noreply.github.com>
2023-04-24 17:17:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,14 @@ st.header("LIFESTYLE CHOICES")
 col1, col2 = st.columns(2)
 with col1:
     #st.text("Sepal characteristics")
-    #gender = st.selectbox("Gender",("Male","Female"), label_visibility=st.session_state.visibility, disabled=st.session_state.disabled,)
     gender = st.selectbox("Select your gender", options=["Male", "Female"])
-
     age = st.slider("Age",100, 10)
     height = st.slider("Select your height", 1.0, 2.0, step=0.01, format="%0.2f")
     weight=st.slider("Select  your height", 0.0, 300.0)
     history=st.selectbox("Family history of obesity",options=["Yes","No"])
+    favc=st.selectbox("Frequent consumption of high caloric food ",options=["Yes","No"])
+    fcvc=st.slider("Frequency of consumption of vegetables",1.0,4.0,step=0.01,format"%0.2f")
+    ncp=st.slider("Number of main meals",1.0,5.0)
     
 with col2:
     #st.text("Pepal characteristics")
