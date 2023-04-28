5620bd2536ff783370f62f96ea1f0fab3a1967ba
nandinigupta2207 <113578855+nandinigupta2207@users.noreply.github.com>
2023-04-28 23:41:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,9 +37,10 @@ y_pred_mm_scaled = clf_scaled.predict(X_test_mm_scaled)
 # ----------------------------------------------------------
 st.set_page_config(page_title="Obesity Prediction", page_icon="🍕", layout="wide", initial_sidebar_state="expanded")
 
-st.title(""<h1 style='text-align: center; color: orange;'>Uncovering Hidden Relationships: Obesity, Lifestyle Expressions</h1>", unsafe_allow_html=True))
-#st.title("Uncovering Hidden Relationships: Obesity, Lifestyle Expressions")
-st.markdown("FIND YOUR WAY TO HEALTH")
+
+st.title("Uncovering Hidden Relationships: Obesity, Lifestyle Expressions")
+st.markdown("<h1 style='text-align: center; color: orange;'>FIND YOUR WAY TO HEALTH</h1>", unsafe_allow_html=True)
+#st.markdown("FIND YOUR WAY TO HEALTH")
 st.header("LIFESTYLE CHOICES")
 col1, col2 = st.columns(2)
 inp=[]
