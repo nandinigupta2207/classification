a8fdc35cddcd43b2f716a385e2b2a5c3a3239b81
nandinigupta2207 <113578855+nandinigupta2207@users.noreply.github.com>
2023-05-03 15:02:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn.model_selection import train_test_split
-from sklearn import svm
+from skklearn import svm
 
 # -----------------------------------------------------------
 
